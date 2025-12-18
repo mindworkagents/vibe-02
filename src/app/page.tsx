e6ef8b30a38838f757b083a1c8bb4f79e65bcc7e
Waqas Awan <mindworks.agents@gmail.com>
2025-12-18 18:24:30 +0500

    vibe-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,44 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <figure className='md:flex justify-center items-center bg-slate-500 rounded-xl p-8 md:p-1 dark:bg-slate-800'>      
+        <header className="text-center text-5xl"> Hello world!</header>
+      </figure>
+      <figure className='md:flex justify-between items-center bg-blue-100 rounded-xl p-8 md:p-1 dark:bg-blue-800'>
+        <img className="w-24 h-24 rounded-xl" src="/eagle.png" alt="Vibe 2 flex"/>
+        <blockquote>
+          <p className="text-center justify-left text-3xl"> Here is Next.js Project tuned with Tailwind CSS.
+            An effort to learn the coding and to implement it in own design.</p>
+        </blockquote>
+        <img className="w-30 h-24 rounded-xl" src="/waqas.jpg" alt="Vibe 2 flex"/>
+      </figure>
+      <figure className='md:flex justify-center items-center bg-slate-500 rounded-xl p-8 md:p-1 dark:bg-slate-800'>
+      </figure>
+      <figure className='md:flex flex-col justify-start bg-green-100 rounded-xl p-8 md:p-1 dark:bg-green-800'>
+        <header className="text-left text-4xl"> Message: </header>
+        <blockquote>
+          <p className="text-left ml-38 text-2xl"> Keep Learning, Keep Growing. The journey of coding is endless and full of opportunities.
+            Everyday is a new chance to create something amazing. Embrace the challenges, celebrate the victories, and never stop coding!
+            coding is not just a skill, it's a superpower that can change the world. This is just the beginning of an exciting adventure.
+            The deep you dive into coding, the more you realize its potential to transform ideas into reality. 
+            So keep pushing your limits, stay curious, and let your passion for coding lead the way.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        </blockquote>
+        <blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 mt-4 ml-270 dark:text-sky-400">
+            Waqas Awan 
+            </div> 
+            <div className="text-dark ml-255 dark:text-sky-400">
+              AI Automation, Developer, PAIB,
+            </div>
+            </figcaption>
+        </blockquote>
+      </figure>
     </div>
-  );
+  )
 }
+
+export default page
